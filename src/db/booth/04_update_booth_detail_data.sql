c1836cade666fcb8733a3be53d16220adc9dc498
hiseyeon <se020122@naver.com>
2026-09-27 01:46:50 +0900

    feat: 솜컬렉션 전체 위치 이미지 연결 (#16)

--- src/db/booth/04_update_booth_detail_data.sql
+++ src/db/booth/04_update_booth_detail_data.sql
@@ -4,10 +4,28 @@ USE dw26areteum;
 -- 03_add_booth_detail_schema.sql 적용 후 실행합니다.
 -- 부스 기본 데이터와 운영 일정이 먼저 입력되어 있어야 합니다.
 
--- 달팽이잡화점(id: 21)의 위치 이미지
--- 양일 모두 솜컬렉션 지도 번호 1번이며,
--- 지도 번호 1·2번은 같은 위치 이미지(1_1.png)를 사용합니다.
-UPDATE booth_operation
-SET location_image_path = '/images/booth-locations/1_1.png'
-WHERE booth_id = 21
-  AND operation_date IN ('2026-09-29', '2026-09-30');
+-- 솜컬렉션 위치 이미지 연결
+-- 부스 ID를 기준으로 전달받은 이미지 파일과 연결합니다.
+-- 양일 운영하는 부스는 두 날짜에 같은 이미지를 사용합니다.
+UPDATE booth_operation o
+    JOIN booth b ON b.id = o.booth_id
+    SET o.location_image_path =
+        CASE
+        WHEN b.id IN (21, 22)
+        THEN '/images/booth-locations/1_1.png'
+        WHEN b.id IN (23, 24, 32)
+        THEN '/images/booth-locations/1_2.png'
+        WHEN b.id IN (25, 26, 27)
+        THEN '/images/booth-locations/1_3.png'
+        WHEN b.id IN (28, 29)
+        THEN '/images/booth-locations/1_4.png'
+        WHEN b.id IN (30, 31, 33)
+        THEN '/images/booth-locations/1_5.png'
+END
+WHERE b.category = 'SOM_COLLECTION'
+  AND b.id IN (
+      21, 22, 23, 24, 25, 26, 27,
+      28, 29, 30, 31, 32, 33
+  )
+  AND o.operation_date IN ('2026-09-29', '2026-09-30');
+
